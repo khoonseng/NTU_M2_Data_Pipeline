2d101d30f5208c2bfec52676d5dc8c0ac6d76d38
khoonseng <khoonseng87@gmail.com>
2026-03-07 22:19:52 +0800

    add dim_date seed with schema validations add macro to convert timestamp to date_key <YYYYMMDD> format update project.yml to define dim_date schema

--- london_bicycles_dbt/models/star/fact_hire.sql
+++ london_bicycles_dbt/models/star/fact_hire.sql
@@ -6,5 +6,7 @@ select
     end_date,
     start_station_id,
     end_station_id,
-    EXTRACT(HOUR FROM start_date) AS start_hour
-from  {{ ref('stg_cycle_hire_valid_stations') }}
+    EXTRACT(HOUR FROM start_date) AS start_hour,
+    {{ generate_date_key('start_date') }} as start_date_key,
+    COALESCE({{ generate_date_key('end_date') }}, 99990101) as end_date_key
+from {{ ref('stg_cycle_hire_valid_stations') }}
