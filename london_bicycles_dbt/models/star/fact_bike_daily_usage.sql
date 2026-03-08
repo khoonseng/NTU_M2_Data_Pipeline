9667db6de5ba22b051ab334849df89c730ffd604
khoonseng <khoonseng87@gmail.com>
2026-03-08 15:32:49 +0800

    update partitioning by date_value column

--- london_bicycles_dbt/models/star/fact_bike_daily_usage.sql
+++ london_bicycles_dbt/models/star/fact_bike_daily_usage.sql
@@ -2,13 +2,9 @@
   config(
     materialized='table',
     partition_by={
-      "field": "date_key",
-      "data_type": "int64",
-      "range": {
-        "start": 20150101,
-        "end": 20230131,
-        "interval": 1
-      }
+      "field": "date_value",
+      "data_type": "date",
+      "granularity": "day"
     },
     cluster_by = ["bike_id"]
   )
@@ -23,7 +19,8 @@ bike_date_combined as (
         {# Generate a surrogate key based on the grain (bike + date) #}
         {{ dbt_utils.generate_surrogate_key(['b.bike_id', 'd.date_key']) }} as bike_usage_key,
         d.date_key,
-        b.bike_id
+        b.bike_id,
+        d.date_value
     FROM {{ ref('dim_bike') }} b
     CROSS JOIN {{ ref('dim_date') }} d
     inner join bikes_hired h on h.bike_id = b.bike_id
@@ -33,14 +30,16 @@ daily_hire_info as (
     select bike_id, 
             start_date_key, 
             count(*) as hire_count,
-            sum(duration_minutes) as daily_duration_minutes
+            sum(duration_minutes) as daily_duration_minutes,            
     from {{ ref('fact_hire') }}
     group by bike_id, start_date_key
 )
 select c.bike_usage_key,
         c.bike_id,
         c.date_key,
+        c.date_value,
         COALESCE(i.hire_count, 0) as hire_count,
-        COALESCE(i.daily_duration_minutes, 0) as daily_duration_minutes
+        COALESCE(i.daily_duration_minutes, 0) as daily_duration_minutes,
+        SUM(COALESCE(daily_duration_minutes, 0)) OVER (PARTITION BY c.bike_id ORDER BY c.date_key) as cumulative_duration_minutes
 from bike_date_combined c
 left outer join daily_hire_info i on c.bike_id = i.bike_id and c.date_key = i.start_date_key
